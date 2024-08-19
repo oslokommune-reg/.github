5c3f550066f2a8e00e2b84803ecbfada9c1db4cd
samuelberntzen <samuel.berntzen@protonmail.com>
2024-08-19 11:35:17 +0200

    added testfiles

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+# Provider must be given in each workspace.
+# Input: additional providers if used
+provider "aws" {
+  region="eu-west-1"
+}
+terraform {
+  required_providers {
+    archive= ">= 1.3"
+    aws= "~> 5.62"
+    local= "~> 1.4"
+  }
+}
+data "terraform_remote_state" "domain" {
+  backend ="remote"
+  config= { organization = var.tfc_organization_name
+    workspaces = {
+      name = "ELT-weights-domain"
+    }
+  }
+}
