e01b19a291368748893a21baffd4f65f4db3aa0e
samuelberntzen <samuelberntzen@users.noreply.github.com>
2024-08-19 10:48:25 +0000

    Applied formatting

--- tests/formatting/test.py
+++ tests/formatting/test.py
@@ -1,5 +1,8 @@
 def myfunction(arg1, arg2, arg3):
     print("hello world")
 
-    mydict = {"arg1": arg1,"arg2": arg2,"arg3": arg3,
+    mydict = {
+        "arg1": arg1,
+        "arg2": arg2,
+        "arg3": arg3,
     }
